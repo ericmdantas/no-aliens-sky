d70e00b2577ba046e46cf92d1f24ccd20f83bc8e
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-10-21 08:10:24 -0200

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,34 @@
 const path = require('path');
 
-module.exports = (env) => {
-    return {
-        entry: './client/dev/index.js',
-        output: {
-            filename: 'bundle.js',
-            path: './client/dev/',
-            publicPath: '/client/dev'
-        },
-        module: {
-            loaders: [
-                {
-                    test: /\.css$/,
-                    loader: 'style!css!'
-                },
-                {
-                    test: /\.js$/,
-                    loader: 'babel',
-                    exclude: /(node_modules)/
-                },
-                {
-                    test: /\.html$/,
-                    loader: 'html'
-                },
-                {
-                    test: /\.(jpe?g|png|gif|svg)$/i,
-                    loaders: [
-                        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                    ]
-                }
-            ]
-        }
+module.exports = {
+    entry: './client/dev/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: './client/dev/',
+        publicPath: '/client/dev'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: 'style!css!'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /(node_modules)/
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
+            }
+        ]
     }
-
 }
