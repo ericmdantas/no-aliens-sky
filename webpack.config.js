99d1215fb65770ad0f20c59439c4ba32b365e93b
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-02-24 15:54:08 -0300

    .

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => {
+  return {
     entry: './client/dev/index.js',
     output: {
         filename: 'bundle.js',
-        path: './client/dev/',
+        path: '/',
         publicPath: '/client/dev'
     },
     resolve: {
@@ -16,24 +17,18 @@ module.exports = {
         loaders: [
             {
                 test: /\.css$/,
-                loader: 'style!css!'
+                loader: 'style-loader!css-loader!'
             },
             {
                 test: /\.js$/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 exclude: /(node_modules)/
             },
             {
                 test: /\.html$/,
-                loader: 'html'
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-                ]
+                loader: 'html-loader'
             }
         ]
     }
+  }
 }
